c7b8a51e8d1dbc7cff78ba31d1a6f610c6b30a3d
stanislav sergachev <89036192606@yandex.ru>
2023-09-21 09:22:40 +0300

    Добавьте файлы проекта.

---
+++ PythonApplication1.py
@@ -0,0 +1,22 @@
+
+
+# Стандартный ввод и вывод
+first_name = input("Input your first name: ")
+last_name=input("Input your last name: ")
+middle_name =input("Input your middle name: ")
+
+print ("Hello ", first_name,"!",sep="")
+
+#Вывод f - строк
+print (f"Hello, {last_name} {first_name} {middle_name}")
+
+number = input ("Input number")
+
+print ("Standart output ",number)
+
+print ("F-string output 0000#")
+print (number)
+print ("F-string output #0000")
+print (number)
+print ("F-string output 00#00")
+print (number)
